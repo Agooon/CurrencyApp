96957d8b7dd5a16dedce715bc033d6940483057a
Agooon <agonplp@gmail.com>
2020-12-20 17:46:02 +0100

    Finished Register page, no w correctly creates and assigns basic role.

---
+++ CurrencyApp/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} musi posiadać między {2} a {1} ilośc znaków.", MinimumLength = 6)]
+        [Display(Name = "Nazwa użytkownika")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} musi posiadać między {2} a {1} ilośc znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła do siebie nie pasują.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- CurrencyApp/Pages/Account/Register.cshtml.cs
+++ CurrencyApp/Pages/Account/Register.cshtml.cs
@@ -16,6 +16,8 @@ using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using CurrenycAppDatabase.DataAccess;
 using CurrencyAppDatabase.Models.AdminPanel;
+using CurrencyApp.Models.Account;
+using Microsoft.Extensions.Configuration;
 
 namespace CurrencyApp.Pages.Account
 {
@@ -25,46 +27,27 @@ namespace CurrencyApp.Pages.Account
         private readonly SignInManager<AppUser> _signInManager;
         private readonly UserManager<AppUser> _userManager;
         private readonly CurrencyContext _context;
+        private readonly IConfiguration _configuration;
 
         public RegisterModel(
             UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager,
-            CurrencyContext context)
+            CurrencyContext context,
+            IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
+            _configuration = configuration;
         }
 
         [BindProperty]
-        public InputModel Input { get; set; }
+        public RegisterViewModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
+        public string RegistrationCode { get; set; }
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        public class InputModel
-        {
-            [Required]
-            [Display(Name = "Username")]
-            public string Username { get; set; }
-
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-        }
 
         public async Task OnGetAsync(string returnUrl = null, string registrationCode = "")
         {
@@ -94,7 +77,7 @@ namespace CurrencyApp.Pages.Account
                     }
                 }
                 ReturnUrl = returnUrl;
-                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+                RegistrationCode = registrationCode;
             }
         }
 
@@ -104,18 +87,16 @@ namespace CurrencyApp.Pages.Account
             if (!Guid.TryParse(registrationCode, out guidCode))
             {
                 TempData["ErrorString"] = "Podano nieprawidłowy format linku rejestracyjny";
-                return Page();
+                return RedirectToPage("Register", new { returnUrl, registrationCode });
             }
             RegistrationCode regCode = await _context.RegistrationCodes.FindAsync(guidCode);
             if (regCode == null)
             {
                 TempData["ErrorString"] = "Podano nieprawidłowy/zużyty kod rejestracyjny";
-                return Page();
+                return RedirectToPage("Register", new { returnUrl, registrationCode });
             }
-                
 
             returnUrl = returnUrl ?? Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
                 var user = new AppUser { UserName = Input.Username, Email = Input.Email, CreatedAt = DateTime.Now, SubscriptionUntil = DateTime.Now.AddDays(1), LimitOfTables = 10 };
@@ -123,15 +104,10 @@ namespace CurrencyApp.Pages.Account
                 if (result.Succeeded)
                 {
                     _context.RegistrationCodes.Remove(regCode);
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                    //    protocol: Request.Scheme);
+                    await _context.SaveChangesAsync();
+                    await _userManager.AddToRoleAsync(user, _configuration.GetSection("RoleList").Get<string[]>()[1]);
+                    await _signInManager.SignInAsync(user, isPersistent: false);                    
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
@@ -140,6 +116,8 @@ namespace CurrencyApp.Pages.Account
                 }
             }
 
+            RegistrationCode = registrationCode;
+            ReturnUrl = returnUrl;
             // If we got this far, something failed, redisplay form
             return Page();
         }
